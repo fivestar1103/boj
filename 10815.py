9e6dcb1b56cfbdedcbcda51d4d03d00f255ff650
Seonghwan Oh <fivestar1103@uos.ac.kr>
2022-09-24 11:35:10 +0900

    second commit

--- 10815.py
+++ 10815.py
@@ -5,4 +5,3 @@ m = int(sys.stdin.readline().rstrip('\n'))
 scan = list(map(int, sys.stdin.readline().rstrip('\n').split()))
 for i in scan:
     print(1 if i in hashMap else 0, end=' ')
-# memo for commiting
